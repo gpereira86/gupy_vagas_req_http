ddbd04aca037a6bec512de23fb4b314995797b73
Glauco Pereira <glaucoadm@gmail.com>
2025-03-05 14:04:35 -0300

    Refatorando

--- __init__.py
+++ __init__.py
@@ -15,56 +15,57 @@ def verificar_planilha_aberta(file_path):
         return True
 
 
-file_path = "vagas.xlsx"
+if __name__ == "__main__":
+    file_path = "vagas.xlsx"
 
-if verificar_planilha_aberta(file_path):
-    print("A planilha está aberta. Por favor, feche-a e tente novamente.")
-    exit()
+    if verificar_planilha_aberta(file_path):
+        print("A planilha está aberta. Por favor, feche-a e tente novamente.")
+        exit()
 
-setParams = {
-    "estagioRemoto": {
-        "jobName": "estagio",
-        "workplaceType": "remote"
-    },
-    "estagioRJ":{
-        "jobName": "estagio",
-        "state": "Rio de Janeiro"
-    },
-    "estagiarioRemoto": {
-        "jobName": "estagiario",
-        "workplaceType": "remote"
-    },
-    "estagiarioRJ":{
-        "jobName": "estagiario",
-        "state": "Rio de Janeiro"
-    },
-    "estagiariaRemoto": {
-        "jobName": "estagiaria",
-        "workplaceType": "remote"
-    },
-    "estagiariaRJ": {
-        "jobName": "estagiaria",
-        "state": "Rio de Janeiro"
-    },
-    "phpRemoto": {
-        "jobName": "PHP",
-        "workplaceType": "remote"
-    },
-    "phpRJ":{
-        "jobName": "PHP",
-        "state": "Rio de Janeiro"
-    },
-}
+    setParams = {
+        "estagioRemoto": {
+            "jobName": "estagio",
+            "workplaceType": "remote"
+        },
+        "estagioRJ":{
+            "jobName": "estagio",
+            "state": "Rio de Janeiro"
+        },
+        "estagiarioRemoto": {
+            "jobName": "estagiario",
+            "workplaceType": "remote"
+        },
+        "estagiarioRJ":{
+            "jobName": "estagiario",
+            "state": "Rio de Janeiro"
+        },
+        "estagiariaRemoto": {
+            "jobName": "estagiaria",
+            "workplaceType": "remote"
+        },
+        "estagiariaRJ": {
+            "jobName": "estagiaria",
+            "state": "Rio de Janeiro"
+        },
+        "phpRemoto": {
+            "jobName": "PHP",
+            "workplaceType": "remote"
+        },
+        "phpRJ":{
+            "jobName": "PHP",
+            "state": "Rio de Janeiro"
+        },
+    }
 
-all_vacancies = []
+    all_vacancies = []
 
-for key, params in setParams.items():
-    print(f"Gerando dados de {key} => {params}")
-    vacancies = vagas.fetch_vacancies(params)
-    all_vacancies.extend(vacancies)
+    for key, params in setParams.items():
+        print(f"Gerando dados de {key} => {params}")
+        vacancies = vagas.fetch_vacancies(params)
+        all_vacancies.extend(vacancies)
 
-plan.update_vacancies_excel(all_vacancies)
+    plan.update_vacancies_excel(all_vacancies)
 
-palavras_chave = ['php', 'desenvolvimento', 'desenvolvedor', 'junior', 'jr']
+    palavras_chave = ['php', 'desenvolvimento', 'desenvolvedor', 'junior', 'júnior', 'jr']
 
-kw.formatar_palavras_chave(file_path, palavras_chave)
+    kw.formatar_palavras_chave(file_path, palavras_chave)
